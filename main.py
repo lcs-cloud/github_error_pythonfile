edf6bd2d02c2377c6a87e9c93f19de9ace1527df
lcs-cloud <59048243+lcs-cloud@users.noreply.github.com>
2020-01-20 16:03:04 +0800

    增加文件
    增加文件：main.py，requirements.txt

---
+++ main.py
@@ -0,0 +1,18 @@
+#coding=utf-8  
+__author__ = 'cloudtogo'
+
+from flask import Flask
+import sys
+
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+
+
+print("test nono"
+
+@app.route('/')
+def hello():
+    return '恭喜您成功地发布了一个运行在云端的python程序 hello-SVN'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080)
